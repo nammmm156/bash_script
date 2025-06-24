0c95d34a2599b567f3c4df57e2f22f0a5ebd022d
nammmm156 <namduongduc123@gmail.com>
2025-06-24 22:39:49 +0700

    fix backup

--- backup.sh
+++ backup.sh
@@ -2,9 +2,11 @@
 
 sourcedir="/home/ducnam"
 desdir="/backup/tarfile"
-expire=10
+expire=10 
 time=$(date +"%Y-%m-%d_%H-%M-%S")
-name="backup_$(time).tar.gz"
-path="$(desdir)/$(name)"
+name="backup_${time}.tar.gz"
+path="${desdir}/${name}"
 mkdir -p "$desdir"
+echo "Starting backup of ${sourcedir} to ${path}"
 tar -czvf "$path" "$sourcedir"
+echo "Backup finished successfully."
